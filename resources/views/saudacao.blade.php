9af01a2781d89737c9fd89b297807760841b8a20
php 03 <php.03@caverna.cec.ime.usp.br>
2018-01-30 22:16:16 -0200

    laravel

---
+++ resources/views/saudacao.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Saudacao</title>
+  </head>
+  <body>
+    @for ($i=0; $i < 10; $i++)
+      <h1>Saudacao</h1>
+    @endfor
+  </body>
+</html>
